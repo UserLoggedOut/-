a7a2894b70c0965c36ecadd52a8e2b3a6d1c7471
吴超 <15835767493@163.com>
2020-03-30 17:23:31 +0800

    全景图

---
+++ make_fangchan/panorama/panorama.js
@@ -0,0 +1,16 @@
+Page({
+    data: {},
+    onLoad: function(n) {
+        var o = n.panorama;
+        this.setData({
+            panorama: o
+        });
+    },
+    onReady: function() {},
+    onShow: function() {},
+    onHide: function() {},
+    onUnload: function() {},
+    onPullDownRefresh: function() {},
+    onReachBottom: function() {},
+    onShareAppMessage: function() {}
+});
