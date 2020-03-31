1bf865eec5037c5aba730714c2162a75ece9b693
吴超 <15835767493@163.com>
2020-03-31 13:03:17 +0800

    /

---
+++ app.js
@@ -0,0 +1,8 @@
+App({
+    util: require("we7/resource/js/util.js"),
+    siteInfo: require("siteinfo.js"),
+    globalData: {
+        font_color: "",
+        ba_color: ""
+    }
+});
---
+++ siteinfo.js
@@ -0,0 +1,10 @@
+var i = {
+    uniacid: "16",
+    acid: "16",
+    multiid: "0",
+    version: "1.0.0",
+    siteroot: "https://www.zheyitianshi.com/app/index.php",
+    token: 12312
+};
+
+module.exports = i;
